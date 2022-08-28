2b2fbcadb9dc839cce45dcc0daf66d51f0e7649c
Anna Kudryavtseva <annakudryavtseva@MacBook-Air-Anna.local>
2022-08-28 11:08:07 +0700

    meditation

---
+++ waves/waves.js
@@ -0,0 +1,38 @@
+const button = document.querySelector("#myButton");
+const audio = document.querySelector("#player");
+
+button.addEventListener("click", function() {
+document.querySelector("#player").play();
+document.querySelector("#myVideo").play();
+})
+
+
+
+const timer = 1;
+let amountTime = timer * 300;
+
+function calculateTime() {
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    if (amountTime < 0){
+        stopTimer();
+        amountTime = 0;
+    }
+
+    function stopTimer() {
+        clearInterval(timerId);
+    }
+}
+
+
+let timerId = setInterval(calculateTime, 1000);
+
